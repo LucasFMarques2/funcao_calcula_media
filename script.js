f7e4b5928c03a9c8e8da46d63ad94f7d18f3b336
Lucas Freitas Marques <117478158+LucasFMarques2@users.noreply.github.com>
2024-01-10 21:45:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
-/* 
-Solicitar o nome do aluno e as 3 notas do bimestre e caucular a me´dia daquele aluno
+const students = [
+    {
+        name: "Joao",
+        noteOne: 6,
+        noteTwo: 7,
+    },
+    {
+        name: "Julia",
+        noteOne: 9,
+        noteTwo: 8,
+    },
+    {
+        name: "Guilherme",
+        noteOne: 3,
+        noteTwo: 2,
+    },
+    {
+        name: "Catarina",
+        noteOne: 8,
+        noteTwo: 6,
+    },
+];
 
-se o aluno passo no bimestre, dar os  parabèns
 
-se o anulo não passou no bimestre, motivar o aluno a dar seu melhor na prova de recuperação
-
-em ambos os casos, mostre uma mesangem com o nome do aluno e a nota
-*/
-
-
-const name = prompt("Informe seu nome:")
-const firstNote = Number(prompt("Informe a primeira nota:"))
-const secondNote = Number(prompt("Informe a segunda nota:"))
-const thirdNote = Number(prompt("Informe a terceira nota:"))
-
-
-const result = (firstNote + secondNote + thirdNote) / 3
-
-result = result.toFixed(2)
-
-if(result >= 6){
-    alert("Parabéns " + name + " você foi aprovado com uma média de " + result)
-}else{
-    alert("Infelizmente você não passou " + name + " sua média foi de " + result + ", mas continue estudado que na proxima você passa!")
+const media = (nota1, nota2) => {
+    return((nota1 + nota2) / 2)
 }
+
+
+const mensagemAlunos = (alunos) =>{
+    let resultado = media(alunos.noteOne, alunos.noteTwo)
+
+    if(resultado < 7){
+        return(`A média do(a) aluno(a) ${alunos.name} é: ${resultado}\nNão foi dessa vez, ${alunos.name}! Tente Novamente!`)
+    }else{
+        return(`A média do(a) aluno(a) ${alunos.name} é: ${resultado}\nParabéns, ${alunos.name}! Você foi aprovado(a) no concurso!`)
+    }
+}
+
+
+for(let estudante of students){
+    let mensagem = mensagemAlunos(estudante)
+    alert(mensagem)
+}
+
